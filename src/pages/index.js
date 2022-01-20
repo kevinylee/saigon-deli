112797014f15331df47b19ba8816a7695f543d42
Kevin Lee <kevinlee@Kevins-MacBook-Pro-2.local>
2022-01-20 12:49:43 -0800

    changed font

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,9 +10,9 @@ import "./index.css"
 const IndexPage = () => {
   return (
     <main>
-      <link rel="preconnect" href="https://fonts.googleapis.com">
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-      <link href="https://fonts.googleapis.com/css2?family=Ruda&display=swap" rel="stylesheet">
+      <style>
+        @import url('https://fonts.googleapis.com/css2?family=Ruda&display=swap');
+      </style>
       <title>Saigon Deli</title>
       <body>
         <header>
