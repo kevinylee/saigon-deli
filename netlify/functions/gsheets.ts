19779e4a74e43236ab03618b9cdcfae90759673b
Aaron <ilestkempo@gmail.com>
2022-03-26 18:57:29 -0700

    formated private key

--- netlify/functions/gsheets.ts
+++ netlify/functions/gsheets.ts
@@ -5,9 +5,11 @@ const handler: Handler = async (event, context) => {
   // Initialize the sheet - doc ID is the long id in the sheets URL
   const doc = new GoogleSpreadsheet('1GneLscLi_f5Xc4moIaIQm5DdDALuP1JtFqOtUhy9yAo');
 
+  const formattedKey = process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: formattedKey,
   });
   
   const res = await doc.loadInfo().then(async () => {
