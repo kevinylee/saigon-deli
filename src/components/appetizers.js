690468c714f0fea2f34d023c009c93c43b8a2674
Aaron <ilestkempo@gmail.com>
2022-06-22 20:05:33 -0700

    a start

--- src/components/appetizers.js
+++ src/components/appetizers.js
@@ -1,8 +1,5 @@
 import * as React from "react"
-import Spring from "../images/saigonFoodPics/saigon_deli_spring_roll_with_shrimp.jpg"
-import Rolls from "../images/saigonFoodPics/saigon_deli_Eggroll.jpg"
-import Combo from "../images/saigonFoodPics/saigon_deli_Combo_Sandwich_with_Wonton_soup.jpg"
-
+import { StaticImage } from "gatsby-plugin-image"
 
 const Appetizers = (props) => {
     return (
@@ -12,31 +9,31 @@ const Appetizers = (props) => {
                 <div className="name-desc">
                     {
                         props.a.map(ele => (
-                            <React.Fragment>
+                            <div>
                               <div className="item-price">
                                 <p className="appename">{ele.Title}</p>
                                 <p>{ele.Price}</p>
                               </div>
                               <p className="desc">{ele.Description}</p>
-                            </React.Fragment>
+                            </div>
                         ))
                     }
                 </div>
             </div>
             <div className="pics">
               <div className="img-desc">
-                <img src={Spring} className="pho-img" alt="spring-rolls"></img>
+                <StaticImage src="../images/saigonFoodPics/saigon_deli_spring_roll_with_shrimp.jpg" className="pho-img" alt="Spring Rolls" />
                 <p className="desc">1. Spring Rolls - Goi Cuon</p>
               </div>
               <div className="img-desc">
-                <img src={Rolls} className="pho-img" alt="egg-rolls"></img>
+                <StaticImage src="../images/saigonFoodPics/saigon_deli_Eggroll.jpg" className="pho-img" alt="Egg Rolls" />
                 <p className="desc">2. Vegetarian Egg Rolls</p>
               </div>
             </div>
             <div className="pics">
               <div className="img-desc">
-                <img src={Combo} className="pho-img" alt="sandwich-wonton"></img>
-                <p className="desc">3A. COMBO: Sandwich & Wonton soup</p>
+              <StaticImage src="../images/saigonFoodPics/saigon_deli_Combo_Sandwich_with_Wonton_soup.jpg" className="pho-img" alt="Banh Mi & Wonton Soup" />
+              <p className="desc">3A. COMBO: Sandwich & Wonton soup</p>
               </div>
             </div>
         </div>
