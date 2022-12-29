cc6397ebe7d47db176fa8b0002692365dd664a3e
Aaron <ilestkempo@gmail.com>
2022-12-28 18:12:16 -0800

    use tips

--- src/components/StickyCheckout.js
+++ src/components/StickyCheckout.js
@@ -4,7 +4,7 @@ import './checkout.scss';
 
 function StickyCheckout({ tips, cart, canOrder, allowCheckout, handleCheckout, onQuantityUpdate }) {
 
-  const tipObject = null;
+  const tipObject = tips[0];
 
   const cartSize = () => {
     console.log(cart);
@@ -21,10 +21,10 @@ function StickyCheckout({ tips, cart, canOrder, allowCheckout, handleCheckout, o
     <div className="callout-wrapper">
       <div className="callout">
         <div className="content">
-          {tipObject && <span style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 12 }}>
+          <span style={{ display: "flex", flexDirection: "row", alignItems: "center", gap: 12 }}>
             <p>Tip Jar <span style={{ color: "#656565" }}>(in $)</span>: </p>
             <QuantitySelection id={tipObject.PriceId} onQuantityUpdate={(a) => onQuantityUpdate(a)} />
-          </span>}
+          </span>
           {allowCheckout && 
             <button disabled={!canOrder} className="checkout-button" onClick={handleCheckout}>Checkout (<b>{cartSize()} items</b>)</button>
           }
