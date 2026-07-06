44c66aa1ec2331171fbad27b28bbbb90e98254fb
Aaron Chen <aaron@gocomplete.ai>
2026-07-06 13:06:10 -0700

    Make gsheets even simpler

--- netlify/functions/gsheets.ts
+++ netlify/functions/gsheets.ts
@@ -1,12 +1,1 @@
-import { Handler } from "@netlify/functions";
-import { handler as menuApi } from "./menu";
-
-if (!process.env.SUPABASE_API_URL || !process.env.SUPABASE_PRIVATE_KEY) {
-  throw "No Supabase credentials founded.";
-}
-
-const handler: Handler = async (event, context) => {
-  return menuApi(event, context);
-};
-
-export { handler };
+export { handler } from "./menu";
