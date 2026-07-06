50c2c17c57dcfda680848271976d36fecd0f383b
Aaron Chen <aaron@gocomplete.ai>
2026-07-06 12:56:48 -0700

    Migrate checkout.ts

--- netlify/lib/clients.ts
+++ netlify/lib/clients.ts
@@ -5,7 +5,13 @@ if (!process.env.SUPABASE_API_URL || !process.env.SUPABASE_PRIVATE_KEY)
   throw new Error("No Supabase credentials founded.");
 
 if (!process.env.STRIPE_SECRET)
-  throw new Error("No Stripe credentials found.");
+  throw new Error("No Stripe secret found.");
+
+if (!process.env.STRIPE_TAX_RATE_ID)
+  throw new Error("No Stripe Tax Rate ID found to calculate sales tax.");
+
+if (!process.env.BASE_URL)
+  throw new Error("No BASE_URL found.");
 
 const supabase = createClient(
   process.env.SUPABASE_API_URL,
